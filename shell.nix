d455596a14a0d0e3a831d6959051746d5fd2fdbc
justinwoo <moomoowoo@gmail.com>
2021-12-13 13:14:50 +0200

    update

--- shell.nix
+++ shell.nix
@@ -1,15 +1,17 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 let
-  easy-ps = import (
-    pkgs.fetchFromGitHub {
-      owner = "justinwoo";
-      repo = "easy-purescript-nix";
-      rev = "0ba91d9aa9f7421f6bfe4895677159a8a999bf20";
-      sha256 = "1baq7mmd3vjas87f0gzlq83n2l1h3dlqajjqr7fgaazpa9xgzs7q";
-    }
-  ) {
-    inherit pkgs;
-  };
+  easy-ps = import
+    (
+      pkgs.fetchFromGitHub {
+        owner = "justinwoo";
+        repo = "easy-purescript-nix";
+        rev = "13ace3addf14dd9e93af9132e4799b7badfbe99e";
+        sha256 = "1gva113kyygjhn9i92vg6cyj15vhyfhq7haq51cvp4xdz4j0q4xn";
+      }
+    )
+    {
+      inherit pkgs;
+    };
 in
 pkgs.mkShell {
   buildInputs =
--- src/groups/spacchetti.libsonnet
+++
@@ -1,7 +0,0 @@
-{
-  metadata: {
-    dependencies: [],
-    repo: 'https://github.com/spacchetti/purescript-metadata.git',
-    version: 'v0.13.6',
-  },
-}
